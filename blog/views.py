e2275eb86849ee039a133e08eab42958784f5fd8
Tan <tan@ix.com>
2023-12-25 22:06:06 +0700

    view data

--- blog/views.py
+++ blog/views.py
@@ -3,7 +3,29 @@ from django.http import HttpResponse
 
 # Create your views here.
 def blog(request):
-    return render(request, 'blog/blog_list.html')
+    context = {
+        'posts': [
+            {
+                "title": "tieu de bai viet 1",
+                "summary": "noi dung tom tat 1",
+            },
+            {
+                "title": "tieu de bai viet 2",
+                "summary": "noi dung tom tat 2",
+            },
+            {
+                "title": "tieu de bai viet 3",
+                "summary": "noi dung tom tat 3",
+            },
+        ]
+    }
+    return render(request, 'blog/blog_list.html', context)
 
 def blog_detail(request):
-    return render(request, 'blog/blog_detail.html')
+    context = {
+        "title": "tieu de bai viet 1",
+        "summary": "noi dung tom tat 1",
+        "body": "noi dung chi tiet 1",
+        "author": "tac gia",
+    }
+    return render(request, 'blog/blog_detail.html', context)
