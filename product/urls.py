00298166ad7dd49af6e4a85dd675c9305e64ef99
Tan <tan@ix.com>
2023-12-28 20:56:08 +0700

    app_name pathname

--- product/urls.py
+++ product/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
 
+app_name = "product"
+
 urlpatterns = [
-    path('', views.product_list),
-    path('detail/<int:id>/', views.product_detail),
+    path('', views.product_list, name="product_list"),
+    path('chi-tiet/<int:id>/', views.product_detail, name="product_detail"),
 ]
