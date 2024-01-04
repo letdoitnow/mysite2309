e96f6790a5624d6d52417cec6a2cfeb56514a579
Tan <tan@ix.com>
2024-01-04 20:24:23 +0700

    admin filter

--- product/admin.py
+++ product/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 from .models import ProductModel
 
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ["product_name", "price", "quantity"]
+    list_filter = ["created_at"]
+    search_fields = ["product_name"]
+
 # Register your models here.
-admin.site.register(ProductModel)
+admin.site.register(ProductModel, ProductAdmin)
