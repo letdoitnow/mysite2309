6ea9c3da1bb3cfc3a38e798e0450dd1414a4f7af
Tan <tan@ix.com>
2024-01-11 20:36:09 +0700

    search

--- product/views.py
+++ product/views.py
@@ -5,40 +5,21 @@ from django.db.models import Q
 
 # Create your views here.
 def product_list(request):
-    # return HttpResponse("Product list")
-    product_list = ProductModel.objects.all().order_by("-created_at")
+    keyword = request.GET.get("keyword")
+    if not keyword:
+        product_list = ProductModel.objects.all()
+    else:
+        product_list = ProductModel.objects.filter(
+            Q(product_name__contains=keyword)
+            | Q(summary__contains=keyword)
+            | Q(description__contains=keyword)
+        )
 
-    # product_list = ProductModel.objects.filter(
-    #     product_name="xiaomi 10", 
-    #     delete_flg=False, 
-    #     quantity=100,
-    #     )
-    
-    # product_list = ProductModel.objects.filter(product_name="xiaomi 10") | ProductModel.objects.filter(delete_flg=False)
-    
-    # product_list = ProductModel.objects.filter( Q(product_name="xiaomi 10") | Q(delete_flg=False) )
-
-    # product_list = ProductModel.objects.filter(product_name__contains="i")
-    # SELECT * FROM ProductModel WHERE product_name LIKE '%xiaomi%'
-    
-    #1 lay cac san pham chua chu 'xi' va chu 'sam' ?
-    # product_list = ProductModel.objects.filter(Q(product_name__contains="xi") & Q(product_name__contains="sam"))
-
-    #2 lay cac san pham chua chu 'xi' hoac chu 'sam' ?
-    # product_list = ProductModel.objects.filter(Q(product_name__contains="xi") | Q(product_name__contains="sam"))
-
-    # product_list = ProductModel.objects.filter(created_at__year__gte=2024)
-
-    #3 lay cac san pham ma ngay tao >=2023 va so luong > 10
-    # product_list = ProductModel.objects.filter(created_at__year__gte=2023, quantity__gt=10)
-    # product_list = ProductModel.objects.filter(created_at__year__gte=2023).filter(quantity__gt=10)
-    # product_list = ProductModel.objects.filter( Q(created_at__year__gte=2023) & Q(quantity__gt=10) )
-
-    # in
-    # product_list = ProductModel.objects.filter(quantity__in=[20, 100])
+    product_list = product_list.order_by("-created_at")
 
     context = {
-        "product_list": product_list
+        "product_list": product_list,
+        "keyword": keyword if keyword else "",
     }
 
     return render(request, "product/product_list.html", context)
