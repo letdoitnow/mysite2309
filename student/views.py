f4bf41041cfaa57ff9c82d35e51df7b06d75734b
Tan <tan@ix.com>
2024-01-18 21:56:39 +0700

    student list

--- student/views.py
+++ student/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import StudentModel
 
 # Create your views here.
+def student_list(request):
+    students = StudentModel.objects.all().order_by("-created_at")
+
+    context = {
+        "students": students
+    }
+    return render(request, 'student/student_list.html', context)
