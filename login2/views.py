bfb0160c4987c843fa8f033314b2bb4becf945b0
Tan <tan@ix.com>
2024-01-25 20:14:32 +0700

    login-3

--- login2/views.py
+++ login2/views.py
@@ -1,8 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
 
 # Create your views here.
 def login2(request):
-    context = {
+    error = ""
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        is_auth = authenticate(username=username, password=password)
+        if is_auth:
+            login(request, is_auth)
+            return redirect("home:home")
+        
+        error = "Tai khoan hoac Mat khau khong dung"
 
+    context = {
+        "error": error
     }
     return render(request, 'login2/login2.html', context)
