e8b880da3295a6c1b15cfa42ed8a88e1501a7740
Tan <tan@ix.com>
2024-01-29 21:12:47 +0700

    api post

--- todos/views.py
+++ todos/views.py
@@ -6,11 +6,21 @@ from .models import TodosModel
 from .serializers import TodosSerializer
 
 # Create your views here.
-@api_view(["GET"])
+@api_view(["GET", "POST"])
 def todos_list(request):
-    model = TodosModel.objects.all()
-    serializer = TodosSerializer(model, many=True)
-    return Response(data=serializer.data)
+    if request.method == "GET":
+        model = TodosModel.objects.all()
+        serializer = TodosSerializer(model, many=True)
+        return Response(data=serializer.data)
+    
+    if request.method == "POST":
+        serializer = TodosSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response()
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
 
 @api_view(["GET"])
 def todos_detail(request, id):
