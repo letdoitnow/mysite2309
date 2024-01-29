df2b1750e2be195daa0e309a52a634a09edb6ef7
Tan <tan@ix.com>
2024-01-29 20:28:34 +0700

    todos model

--- todos/models.py
+++ todos/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class TodosModel(models.Model):
+    title = models.CharField(max_length=100)
+    summary = models.CharField(max_length=500, null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
+    status = models.IntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    delete_flg = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
