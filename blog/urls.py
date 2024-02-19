4d8de91757eebd1c660e5cfde2804d24e14d3a21
Tan <tan@ix.com>
2024-02-19 21:31:24 +0700

    active tab

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
 
+app_name = "blog"
+
 urlpatterns = [
-    path('', views.blog),
-    path('detail/', views.blog_detail),
+    path('', views.blog, name="blog"),
+    path('detail/', views.blog_detail, name="blog_detail"),
 ]
