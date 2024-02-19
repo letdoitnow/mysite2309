65a6bb29705b02b2116b05e4972cc41f1073d90d
Tan <tan@ix.com>
2024-02-19 22:17:19 +0700

    checkout

--- checkout/views.py
+++ checkout/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
+from product.models import ProductModel
+from customer.models import CustomerModel
+
 
 # Create your views here.
 def checkout(request, id):
+    product = ProductModel.objects.get(id=id)
+    customer = CustomerModel.objects.get(user=request.user)
     context = {
-        "id": id
+        "product": product,
+        "customer": customer,
     }
     return render(request, 'checkout/checkout.html', context)
