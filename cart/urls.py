b34bce4dae0eda782393d9fe6c03e3a9cc2c956b
Tan <tan@ix.com>
2024-02-22 21:44:02 +0700

    order

--- cart/urls.py
+++ cart/urls.py
@@ -4,4 +4,5 @@ from . import views
 app_name = "cart"
 urlpatterns = [
     path('', views.cart, name="cart"),
+    path('order/<int:id>/', views.order, name="order"),
 ]
--- cart/views.py
+++ cart/views.py
@@ -27,3 +27,20 @@ def cart(request):
         "order_detail": order_detail,
     }
     return render(request, 'cart/cart.html', context)
+
+def order(request, id):
+    order = OrdersModel.objects.get(id=id)
+    order.status = 1
+    order.description = request.POST.get("description")
+
+    order_detail = OrdersDetailModel.objects.filter(orders=order)
+    for item in order_detail:
+        item.price = item.product.price
+        item.save()
+    
+    order.save()
+
+    context = {
+        "order": order
+    }
+    return render(request, 'cart/order.html', context)
