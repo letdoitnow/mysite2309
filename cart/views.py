b75d13778ceacc098a27888e605d826fb71c2c10
Tan <tan@ix.com>
2024-02-22 20:37:29 +0700

    cart-customer-info

--- cart/views.py
+++ cart/views.py
@@ -2,11 +2,13 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.shortcuts import render
+from customer.models import CustomerModel
 
 
 # Create your views here.
 def cart(request):
+    customer = CustomerModel.objects.get(user=request.user)
     context = {
-        
+        "customer": customer
     }
     return render(request, 'cart/cart.html', context)
